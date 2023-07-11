f308b8188d303b558292c8aa476132572312b827
Samithra <samithrasamarasinghe@gmail.com>
2023-07-11 10:27:44 +0530

    Utill added

---
+++ client/src/utils/links.js
@@ -0,0 +1,33 @@
+import { IoBarChartSharp } from "react-icons/io5";
+import { MdQueryStats } from "react-icons/md";
+import { FaWqforms } from "react-icons/fa";
+import { ImProfile } from "react-icons/im";
+
+const links = [
+  {
+    id: 1,
+    text: "stats",
+    path: "/",
+    icon: <IoBarChartSharp />,
+  },
+  {
+    id: 2,
+    text: "all jobs",
+    path: "/all-jobs",
+    icon: <IoBarChartSharp />,
+  },
+  {
+    id: 3,
+    text: "add job",
+    path: "/add-job",
+    icon: <IoBarChartSharp />,
+  },
+  {
+    id: 4,
+    text: "profile",
+    path: "/profile",
+    icon: <IoBarChartSharp />,
+  },
+];
+
+export default links;
