0aee08aee58e7fbfc0268e4134f4afeeb2447446
Samithra <samithrasamarasinghe@gmail.com>
2023-07-11 10:25:54 +0530

    Navbar component changed

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,8 @@
 import Alert from "./Alert.js";
 import FormRow from "./FormRow.js";
 import Logo from "./Logo.js";
+import Navbar from "./Navbar.js";
+import BigSidebar from "./BigSidebar.js";
+import SmallSidebar from "./SmallSidebar.js";
 
-
-export { Logo,FormRow,Alert };
+export { Logo, FormRow, Alert, SmallSidebar, BigSidebar, Navbar };
