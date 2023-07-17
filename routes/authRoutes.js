fb8346e65207b19bca5d1d95aba8b1e428b506e6
Samithra <samithrasamarasinghe@gmail.com>
2023-07-17 17:16:37 +0530

    make authenticate for update user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import {register,login,updateUser} from '../controler/authControler.js';
+import express from "express";
+import { register, login, updateUser } from "../controler/authControler.js";
+import authenticatedUser from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.route('/register').post(register);
-router.route('/login').post(login);
-router.route('/updateuser').patch(updateUser);
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/updateuser").patch(authenticatedUser, updateUser);
 
 export default router;
