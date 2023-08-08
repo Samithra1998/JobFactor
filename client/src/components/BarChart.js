e7e03e987d3d9285c7b0a63484fe1af2808a91e5
Samithra <samithrasamarasinghe@gmail.com>
2023-08-08 10:16:28 +0530

    Add barchart component

---
+++ client/src/components/BarChart.js
@@ -0,0 +1,24 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const Barchart = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart margin={{ top: 50 }} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey="count" fill="#8884d8" barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+export default Barchart;
