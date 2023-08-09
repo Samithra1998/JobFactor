a14917d9ea1cf482ee054de03c5b5c499ead099c
Samithra <samithrasamarasinghe@gmail.com>
2023-08-09 15:57:13 +0530

    Completed search container

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,6 +1,68 @@
+import { FormRow, FormRowSelect } from ".";
+import { useAppContext } from "../context/appContext";
+import Wrapper from "../assets/wrappers/SearchContainer";
+
 const SearchContainer = () => {
+  const handleSearch = (e) => {
+    if (!isLoading) {
+      handleChange({ name: e.target.name, value: e.target.value });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearFilters()
+  };
+  const {
+    isLoading,
+    search,
+    searchType,
+    searchStatus,
+    sort,
+    sortOptions,
+    statusOptions,
+    jobTypeOptions,
+    handleChange,
+    clearFilters,
+  } = useAppContext();
   return (
-    <div>Search Component</div>
-  )
-}
-export default SearchContainer
+    <Wrapper>
+      <form className="form">
+        <h4>Search Form</h4>
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="search"
+            value={search}
+            handleChange={handleSearch}
+          />
+          <FormRowSelect
+            labelText="status"
+            name="searchStatus"
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={["all", ...statusOptions]}
+          />
+          <FormRowSelect
+            labelText="Job type"
+            name="searchType"
+            value={searchType}
+            handleChange={handleSearch}
+            list={["all", ...jobTypeOptions]}
+          />
+          <FormRowSelect
+            labelText="sort"
+            name="sort"
+            value={sort}
+            handleChange={handleSearch}
+            list={["all", ...sortOptions]}
+          />
+          <button className="btn btn-block btn-danger" onClick={handleSubmit}>
+            Clear
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
+};
+export default SearchContainer;
