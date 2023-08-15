9b28205d4540f0f8d09490d585cf29dec080d333
Samithra <samithrasamarasinghe@gmail.com>
2023-08-15 10:25:20 +0530

    add new route for refresh jobs

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, updateUser } from "../controler/authControler.js";
+import { register, login, updateUser, getCurrentUser } from "../controler/authControler.js";
 import authenticatedUser from "../middleware/auth.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/updateuser").patch(authenticatedUser, updateUser);
+router.route("/getcurrentuser").get(authenticatedUser, getCurrentUser)
 
 export default router;
