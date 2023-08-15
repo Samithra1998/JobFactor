9794ad13b88f9d7b1cfd8a6ef78210c0dba58e6c
Samithra <samithrasamarasinghe@gmail.com>
2023-08-15 11:58:10 +0530

    add logout user routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, updateUser, getCurrentUser } from "../controler/authControler.js";
+import { register, login, updateUser, getCurrentUser, logoutUser } from "../controler/authControler.js";
 import authenticatedUser from "../middleware/auth.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/updateuser").patch(authenticatedUser, updateUser);
 router.route("/getcurrentuser").get(authenticatedUser, getCurrentUser)
+router.route("/logoutuser").get(logoutUser)
 
 export default router;
