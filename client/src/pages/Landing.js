4a67a8930b78fe3efe2b6d8196f060ab5c2814e8
Samithra <samithrasamarasinghe@gmail.com>
2023-08-15 10:28:27 +0530

    secure the url by manual editing

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,32 +1,38 @@
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/Testing";
-import {Logo} from "../components";
-import { Link } from "react-router-dom";
+import { Logo } from "../components";
+import { Link, Navigate } from "react-router-dom";
+import { useAppContext } from "../context/appContext";
+import React from "react";
 
 const Landing = () => {
+  const { user } = useAppContext();
   return (
-    <Wrapper>
-      <nav>
-        <Logo/>
-      </nav>
-      <div className="container page">
-        <div className="info">
-          <h1>
-            Job<span> tracking</span> app
-          </h1>
-          <p>
-            I'm baby art party crucifix truffaut microdosing yes plz disrupt.
-            Waistcoat gastropub pour-over neutra DIY. Kickstarter subway tile
-            hashtag cupping pitchfork disrupt try-hard tumblr deep v. Poutine
-            lyft migas vexillologist everyday carry selvage.
-          </p>
-          <Link to='/register' className='btn btn-hero'>
-            Login/Register
-          </Link>
+    <React.Fragment>
+      {user && <Navigate to="/" />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className="container page">
+          <div className="info">
+            <h1>
+              Job<span> tracking</span> app
+            </h1>
+            <p>
+              I'm baby art party crucifix truffaut microdosing yes plz disrupt.
+              Waistcoat gastropub pour-over neutra DIY. Kickstarter subway tile
+              hashtag cupping pitchfork disrupt try-hard tumblr deep v. Poutine
+              lyft migas vexillologist everyday carry selvage.
+            </p>
+            <Link to="/register" className="btn btn-hero">
+              Login/Register
+            </Link>
+          </div>
+          <img src={main} alt="main_img" className="img main-img" />
         </div>
-        <img src={main} alt='main_img' className='img main-img'/>
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </React.Fragment>
   );
 };
 
