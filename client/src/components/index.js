e2275e11d8293341926fb956b96abd5627067f1f
Samithra <samithrasamarasinghe@gmail.com>
2023-08-15 10:24:07 +0530

    modify exports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -9,12 +9,17 @@ import SearchContainer from "./SearchContainer.js";
 import JobsContainer from "./JobsContainer.js";
 import Job from "./Job.js";
 import JobInfo from "./JobInfo.js";
+import ChartsContainer from "./ChartsContainer.js";
+import StatItem from "./StatItem.js";
+import StatsContainer from "./StatsContainer.js";
+import Loading from "./Loading.js";
 
 export {
   Logo,
   FormRow,
   Alert,
   SmallSidebar,
+  Loading,
   BigSidebar,
   Navbar,
   FormRowSelect,
@@ -22,4 +27,7 @@ export {
   JobsContainer,
   Job,
   JobInfo,
+  ChartsContainer,
+  StatItem,
+  StatsContainer
 };
